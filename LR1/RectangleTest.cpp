47611bc783c78472feceaf92414a970b57fc8677
Negadiay <69913766+Negadiay@users.noreply.github.com>
2024-11-30 13:35:55 +0300

    Update RectangleTest.cpp

--- LR1/RectangleTest.cpp
+++ LR1/RectangleTest.cpp
@@ -1,47 +1,73 @@
-#include <gtest/gtest.h>
-#include "Rectangle.h"
+#include "pch.h"
+#include "../ppois11/Rectangle.h"
+#include "../ppois11/Rectangle.cpp"
 
-// Тестирование конструктора по умолчанию
-TEST(RectangleTests, DefaultConstructor) {
-    Rectangle r;
-    EXPECT_EQ(r.getBottomLeft().x, 0);
-    EXPECT_EQ(r.getBottomLeft().y, 0);
-    EXPECT_EQ(r.getTopRight().x, 1);
-    EXPECT_EQ(r.getTopRight().y, 1);
+TEST(RectangleTest, DefaultConstructor) {
+    Rectangle rect;
+    EXPECT_EQ(rect.getBottomLeft().x, 0);
+    EXPECT_EQ(rect.getBottomLeft().y, 0);
+    EXPECT_EQ(rect.getTopRight().x, 1);
+    EXPECT_EQ(rect.getTopRight().y, 1);
 }
 
-// Тестирование параметризованного конструктора
-TEST(RectangleTests, ParameterizedConstructor) {
-    Rectangle r(2, 3, 4, 5);
-    EXPECT_EQ(r.getBottomLeft().x, 2);
-    EXPECT_EQ(r.getBottomLeft().y, 3);
-    EXPECT_EQ(r.getTopRight().x, 7); // 2 + 5
-    EXPECT_EQ(r.getTopRight().y, 7); // 3 + 4
+TEST(RectangleTest, ParameterizedConstructor) {
+    Rectangle rect(1, 2, 3, 4);
+    EXPECT_EQ(rect.getBottomLeft().x, 1);
+    EXPECT_EQ(rect.getBottomLeft().y, 2);
+    EXPECT_EQ(rect.getTopRight().x, 5);
+    EXPECT_EQ(rect.getTopRight().y, 5);
 }
 
-// Тестирование перемещения прямоугольника
-TEST(RectangleTests, SetNewLocation) {
-    Rectangle r(2, 3, 4, 5);
-    r.setNewLocation(10, 15);
-    EXPECT_EQ(r.getBottomLeft().x, 10);
-    EXPECT_EQ(r.getBottomLeft().y, 15);
+TEST(RectangleTest, CopyConstructor) {
+    Rectangle rect1(1, 2, 3, 4);
+    Rectangle rect2(rect1);
+    EXPECT_EQ(rect2.getBottomLeft().x, 1);
+    EXPECT_EQ(rect2.getBottomLeft().y, 2);
+    EXPECT_EQ(rect2.getTopRight().x, 5);
+    EXPECT_EQ(rect2.getTopRight().y, 5);
 }
 
-// Тестирование изменения ширины
-TEST(RectangleTests, ChangeWidth) {
-    Rectangle r(0, 0, 4, 5);
-    r.changeWidth(10);
-    EXPECT_EQ(r.getTopRight().y, 10); // Новая ширина (высота)
+TEST(RectangleTest, SetNewLocation) {
+    Rectangle rect;
+    rect.setNewLocation(5, 6);
+    EXPECT_EQ(rect.getBottomLeft().x, 5);
+    EXPECT_EQ(rect.getBottomLeft().y, 6);
 }
 
-// Тестирование изменения длины
-TEST(RectangleTests, ChangeLength) {
-    Rectangle r(0, 0, 4, 5);
-    r.changeLength(8);
-    EXPECT_EQ(r.getTopRight().x, 8); // Новая длина
+TEST(RectangleTest, ChangeHeight) {
+    Rectangle rect;
+    rect.changeHeight(10);
+    EXPECT_EQ(rect.getTopLeft().y, 10);
 }
 
-// Тестирование оператора сложения
+TEST(RectangleTest, ChangeLength) {
+    Rectangle rect;
+    rect.changeLength(15);
+    EXPECT_EQ(rect.getBottomRight().x, 15);
+}
+
+TEST(RectangleTest, IncrementDecrementOperators) {
+    Rectangle rect(0, 0, 3, 4);
+    rect++;
+    EXPECT_EQ(rect.getTopRight().x, 5);
+    EXPECT_EQ(rect.getTopRight().y, 4);
+
+    rect--;
+    EXPECT_EQ(rect.getTopRight().x, 4);
+    EXPECT_EQ(rect.getTopRight().y, 3);
+}
+
+TEST(RectangleTest, EqualityOperators) {
+    Rectangle rect1(0, 0, 3, 4);
+    Rectangle rect2(0, 0, 3, 4);
+    Rectangle rect3(0, 0, 5, 6);
+
+    EXPECT_TRUE(rect1 == rect2);
+    EXPECT_FALSE(rect1 == rect3);
+    EXPECT_TRUE(rect1 != rect3);
+    EXPECT_FALSE(rect1 != rect2);
+}
+
 TEST(RectangleTests, AdditionOperator) {
     Rectangle r1(0, 0, 4, 5);
     Rectangle r2(0, 0, 3, 6);
@@ -49,64 +75,49 @@ TEST(RectangleTests, AdditionOperator) {
 
     EXPECT_EQ(result.getBottomLeft().x, 0);
     EXPECT_EQ(result.getBottomLeft().y, 0);
-    EXPECT_GE(result.getTopRight().x, r1.getTopRight().x); // Увеличенная длина
-    EXPECT_GE(result.getTopRight().y, r1.getTopRight().y); // Увеличенная высота
+    EXPECT_GE(result.getTopRight().x, r1.getTopRight().x);
+    EXPECT_GE(result.getTopRight().y, r1.getTopRight().y);
 }
 
-// Тестирование оператора сравнения
-TEST(RectangleTests, EqualityOperator) {
-    Rectangle r1(0, 0, 4, 5);
-    Rectangle r2(0, 0, 4, 5);
-    Rectangle r3(0, 0, 3, 6);
+TEST(RectangleTest, SubtractionOperators) {
+    Rectangle rect1(0, 0, 5, 6);
+    Rectangle rect2(0, 0, 3, 4);
 
-    EXPECT_TRUE(r1 == r2);
-    EXPECT_FALSE(r1 == r3);
+    Rectangle rect3 = rect1 - rect2;
+    EXPECT_EQ(rect3.getTopRight().x, 4);
+    EXPECT_EQ(rect3.getTopRight().y, 3);
+
+    rect1 -= rect2;
+    EXPECT_EQ(rect1.getTopRight().x, 4);
+    EXPECT_EQ(rect1.getTopRight().y, 3);
 }
 
-// Тестирование оператора неравенства
-TEST(RectangleTests, InequalityOperator) {
-    Rectangle r1(0, 0, 4, 5);
-    Rectangle r2(0, 0, 3, 6);
+TEST(RectangleTest, IOOperators) {
+    Rectangle rect;
+    std::istringstream input("1 2 3 4");
+    input >> rect;
 
-    EXPECT_TRUE(r1 != r2);
+    EXPECT_EQ(rect.getBottomLeft().x, 1);
+    EXPECT_EQ(rect.getBottomLeft().y, 2);
+    EXPECT_EQ(rect.getTopRight().x, 5);
+    EXPECT_EQ(rect.getTopRight().y, 5);
+
+    std::ostringstream output;
+    output << rect;
+    std::string expected = "Координата x: 1\nКоордината y: 2\nВысота: 3\nДлина: 4\n";
+    EXPECT_EQ(output.str(), expected);
 }
 
-// Тестирование префиксного оператора увеличения
 TEST(RectangleTests, PrefixIncrement) {
     Rectangle r(0, 0, 4, 5);
     ++r;
-    EXPECT_EQ(r.getTopRight().x, 6); // Длина увеличена
-    EXPECT_EQ(r.getTopRight().y, 5); // Высота увеличена
+    EXPECT_EQ(r.getTopRight().x, 6);
+    EXPECT_EQ(r.getTopRight().y, 5);
 }
 
-// Тестирование постфиксного оператора уменьшения
 TEST(RectangleTests, PostfixDecrement) {
-    Rectangle r(0, 0, 4, 5);
-    r--;
-    EXPECT_EQ(r.getTopRight().x, 4); // Длина уменьшена
-    EXPECT_EQ(r.getTopRight().y, 3); // Высота уменьшена
-}
-
-// Тестирование ввода и вывода
-TEST(RectangleTests, InputOutputOperators) {
-    Rectangle r;
-    std::istringstream input("1 2 3 4");
-    input >> r;
-
-    EXPECT_EQ(r.getBottomLeft().x, 1);
-    EXPECT_EQ(r.getBottomLeft().y, 2);
-    EXPECT_EQ(r.getTopRight().x, 5); // 1 + 4
-    EXPECT_EQ(r.getTopRight().y, 5); // 2 + 3
-}
-
-// Тестирование оператора вычитания
-TEST(RectangleTests, SubtractionOperator) {
-    Rectangle r1(0, 0, 4, 5);
-    Rectangle r2(0, 0, 2, 3);
-    Rectangle result = r1 - r2;
-
-    EXPECT_EQ(result.getBottomLeft().x, 0);
-    EXPECT_EQ(result.getBottomLeft().y, 0);
-    EXPECT_EQ(result.getTopRight().x, 5); // Минимальная длина
-    EXPECT_EQ(result.getTopRight().y, 4); // Минимальная высота
+    Rectangle r(0, 0, 4, 3);
+    --r;
+    EXPECT_EQ(r.getTopRight().x, 2);
+    EXPECT_EQ(r.getTopRight().y, 3);
 }
